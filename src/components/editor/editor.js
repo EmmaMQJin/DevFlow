9beb61cb71c743c1a2276ed352948c070037cb4f
EmmaMQJin <mqjin@seas.upenn.edu>
2024-03-31 09:44:43 -0400

    updating components structure

---
+++ src/components/editor/editor.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './editor.css';
+
+class Editor extends Component {
+  render() {
+    return (
+        <p>© 2024 My React App</p>
+    );
+  }
+}
+
+export default Editor;
---
+++ src/components/outline/outline.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './outline.css';
+
+class Outline extends Component {
+  render() {
+    return (
+        <p>© 2024 My React App</p>
+    );
+  }
+}
+
+export default Outline;
